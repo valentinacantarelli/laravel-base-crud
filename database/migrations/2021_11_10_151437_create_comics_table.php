c485bdf28491c4c4c8a1d5d736a2efaf46178aa2
valentina cantarelli <valentinacanterina@gmail.com>
2021-11-10 16:27:56 +0100

    first commit

---
+++ database/migrations/2021_11_10_151437_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 50);
+            $table->text("description");
+            $table->text("image");
+            $table->float("price", 5, 2);
+            $table->string("series", 30);
+            $table->date("sale_date");
+            $table->string("type", 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
