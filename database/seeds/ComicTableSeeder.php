a09eb1d13269470b6d3a695ca180e6ded89d60e4
valentina cantarelli <valentinacanterina@gmail.com>
2021-11-10 17:11:38 +0100

    aggiunto seeder

---
+++ database/seeds/ComicTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config("comics");
+
+        foreach ($comics as $comic){
+            $newComic = new Comic();
+            $newComic ->title = $comic["title"];
+            $newComic ->description=$comic["description"];
+            $newComic ->image=$comic["thumb"];
+            $newComic ->price=$comic["price"];
+            $newComic ->series=$comic["series"];
+            $newComic ->sale_date=$comic["sale_date"];
+            $newComic ->type=$comic["type"];
+            $newComic -> save();
+        }
+    }
+}
